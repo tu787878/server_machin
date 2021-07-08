42abeb2bf37df93e15193fc428e0c826cddc77be
Văn Tú Nguyễn <vantu@Vans-MBP.fritz.box>
2021-07-08 13:43:18 +0200

    clean

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ var api = "/api/v1.0/";
 
 'use strict'
 
+const extract = require('extract-zip')
 const fs = require('fs')
 const path = require('path')
-const { promisify } = require('util')
 const pcbStackup = require('pcb-stackup')
-const whatsThatGerber = require('whats-that-gerber');
-const writeFile = promisify(fs.writeFile)
-const { validate } = require('whats-that-gerber');
 const GERBERS_DIR = '';
-const TOP_OUT = 'test1.svg';
-const BOTTOM_OUT = 'test2.svg'; //path.join(__dirname, 'arduino-uno-bottom.svg');
+const { v4: uuidv4 } = require('uuid');
 
+var multer = require('multer')
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, './resources/uploads');
+	},
+	filename: function (req, file, cb) {
+		cb(null, getRandomFileName() + '-' + file.originalname);
+	}
+});
+const uploadImg = multer({ storage: storage }).single('file');
 
-
+function getRandomFileName() {
+	return uuidv4();
+}
 
 var DEFAULT_COLOR = {
 	// fr4: '#666',
@@ -39,13 +47,13 @@ var GERBER_FILENAMES = [];
 var typeByFilename;
 //passsing directoryPath and callback function
 
-function json2array(json){
-    var result = [];
-    var keys = Object.keys(json);
-    keys.forEach(function(key){
-        result.push(key);
-    });
-    return result;
+function json2array(json) {
+	var result = [];
+	var keys = Object.keys(json);
+	keys.forEach(function (key) {
+		result.push(key);
+	});
+	return result;
 }
 
 var stackup;
@@ -58,11 +66,76 @@ app.get(api + 'test-server', (req, res, next) => {
 	res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
+app.post(api + 'uploadFile', function (req, res, next) {
+	let file_name;
+	uploadImg(req, res, function (err) {
+		if (err) {
+			console.log(JSON.stringify(err));
+			res.json({
+				status: 0,
+				message: "Error when uploading file zip",
+				data: null
+			});
+		} else {
+			file_name = res.req.file.filename;
+			let id_file = file_name.split('.').slice(0, -1).join('.');
+			let file_folder = "resources/uploads/" + file_name.split('.').slice(0, -1).join('.');
+			console.log('The folder name is ' + file_folder);
+			file_name = "resources/uploads/" + file_name;
+			//console.log(path.resolve(file_folder));
+			extract(file_name, { dir: path.resolve(file_folder), onEntry: (entry, zipfile) => {
+				entry.fileName = entry.fileName.split("/")[1];
+				
+			}}, function (err) {
+				console.log(err);
+				res.json({
+					status: 0,
+					message: "Error when unzip files",
+					data: null
+				});
+			}).then(() => {
+
+				GERBER_FILENAMES = [];
+				renderStackup(file_folder)
+					.then(writeStackup)
+					.then(() => {
+						res.json(
+							{
+								status: 1,
+								message: "success",
+								top: stackup.top.svg,
+								bottom: stackup.bottom.svg,
+								layers: stackup.layers,
+								file_folder: file_folder,
+								id_file: id_file
+							}
+						);
+					})
+					.catch(error => {
+						console.error('Error rendering stackup', error);
+						res.json({
+							status: 0,
+							message: "Wrong File name.",
+							data: null
+						});
+					});
+			}).catch(() => {
+				res.json({
+					status: 0,
+					message: "Chỉ hỗ trợ file zip.",
+					data: null
+				});
+			})
+
+		}
+	});
+
+})
+
 app.get(api + 'test-svg/:fileid', (req, res, next) => {
 	//res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 	let fileid = req.params["fileid"];
 	GERBER_FILENAMES = [];
-	console.log(fileid);
 	renderStackup(fileid)
 		.then(writeStackup)
 		.then(() => {
@@ -89,6 +162,8 @@ app.get(api + 'test-svg/:fileid', (req, res, next) => {
 
 const readFilePromise = (folder) => {
 	const directoryPath = path.join(__dirname, folder);
+	console.log(directoryPath);
+
 	return new Promise((resolve, reject) => {
 		fs.readdir(directoryPath, function (err, files) {
 			//handling error
@@ -96,49 +171,60 @@ const readFilePromise = (folder) => {
 				reject("error");
 				return;
 			}
+
+			console.log(files);
 			//listing all files using forEach
 			files.forEach(function (file) {
 				// Do whatever you want to do with the file
-				console.log(file);
-				if (file != '.DS_Store' && file != 'PCB1 - V3.GKO') {
+				if (file != '.DS_Store' && file != 'PCB123 - V3.GKO' && file.indexOf(".") != -1) {
 					GERBER_FILENAMES.push(folder + "/" + file);
 				}
 			});
 
 			resolve(files);
-		
+
 		})
 	})
-  }
+}
 
-  function returnPromise(file) {
-	return new Promise(function(resolve, reject) {
+function returnPromise(file) {
+	return new Promise(function (resolve, reject) {
 		readFilePromise(file).then(data => {
 			const layers = GERBER_FILENAMES.map(filename => ({
 				filename,
 				gerber: fs.createReadStream(path.join(GERBERS_DIR, filename)),
 			}))
-		
+			console.log(layers);
+
 			resolve(pcbStackup(layers, {
 				color: DEFAULT_COLOR,
-				attributes: {
-					class: 'tudc',
-				},
+				id: 'tudc',
+				outlineGapFill: 3.5,
+				useOutline: true
 			}))
-		}).catch(()=>{
+		}).catch(() => {
 			reject("error");
 		})
 	});
-  }
+}
 
 function renderStackup(file) {
 
 	return returnPromise(file);
-	
+
 }
 
 function writeStackup(st) {
 	//console.log(stackup);
 	stackup = st;
 
+}
+
+async function giainen(source, target) {
+	try {
+		await extract(source, { dir: target })
+		console.log('Extraction complete')
+	} catch (err) {
+		// handle any errors
+	}
 }
